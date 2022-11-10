5ce1fdaaf7e1c43aabe1a0861a932e87406292da
Thijmen Brand <thijmen@ik.nu>
2022-11-10 14:50:36 +0100

    fixed ts module

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { fileIconsPath, host } from "./config";
+import {
+  AccountSettings,
+  AppSettings,
+  ApplicationMetaData,
+  ApplicationMetaDataFields,
+  ApplicationMetaDataObject,
+  Directory,
+  MimeTypes,
+  Mkdir,
+  OSSettings,
+  Path,
+  PersonalisationSettings,
+  SystemSettings,
+} from "./types";
+
+export {
+  fileIconsPath,
+  host,
+  AccountSettings,
+  ApplicationMetaData,
+  AppSettings,
+  ApplicationMetaDataFields,
+  ApplicationMetaDataObject,
+  Directory,
+  MimeTypes,
+  Mkdir,
+  OSSettings,
+  Path,
+  PersonalisationSettings,
+  SystemSettings,
+};
